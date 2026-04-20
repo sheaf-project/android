9ca9285a20c1449a5b48aea5795601666f33a14e
Nocturnal <241546228+fullynocturnal@users.noreply.github.com>
2026-04-20 19:35:39 -0400

    fix: add missing luminance import in Components.kt

--- sheaf/app/src/main/java/systems/lupine/sheaf/ui/components/Components.kt
+++ sheaf/app/src/main/java/systems/lupine/sheaf/ui/components/Components.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
